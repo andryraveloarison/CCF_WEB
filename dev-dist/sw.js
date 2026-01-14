57b39d730175e9d923e6930cc528760f6f9d0b39
Andry <andryraveloarison01@gmail.com>
2026-01-14 12:46:09 +0300

    fixe intallation ios

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r92l5igqbkg"
+    "revision": "0.se3cgufdt9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
