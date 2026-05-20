630a933630176b1db439d41ed694ad1bad2380d7
andry <andryraveloarison01@gmail.com>
2026-05-20 09:16:51 +0300

    fix download

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        navigateFallbackDenylist: [/^\/releases\//],
         runtimeCaching: [
           {
             // 🔒 Match l’API sur Render
